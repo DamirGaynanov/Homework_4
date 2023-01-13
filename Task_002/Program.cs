df4c6c310c24dda0dd2951def1d95668faaeca54
_DG_ <damirgaynanov@gmail.com>
2023-01-13 23:57:04 +0400

    First commit

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!I love you!");
+﻿// Блок запроса входных данных
+Console.WriteLine("Enter the first number_");
+string number1 = Console.ReadLine() ?? "";
+Console.WriteLine("Enter the second number_");
+string number2 = Console.ReadLine() ?? "";
+
+// Блок подготовительный (перевод строки в число)
+int a = int.Parse(number1);
+int b = int.Parse(number2);
+
+// Блок операции (условие-сравнение двух чисел)
+if (a > b)
+{
+    Console.WriteLine("max=" + a);
+}
+else
+{
+    Console.WriteLine("max=" + b);
+}
