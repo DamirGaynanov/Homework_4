1cda9fe31093887872149616806e60985b88e844
_DG_ <damirgaynanov@gmail.com>
2023-01-14 02:01:02 +0400

    Third commit

---
+++ Task_006/Program.cs
@@ -0,0 +1,12 @@
+﻿// Requrst for input data
+Console.WriteLine("Enter the number");
+string number = Console.ReadLine() ?? "";
+
+// Processing of input data
+int a = int.Parse(number);
+
+// Decision
+if (a % 2 == 0) Console.WriteLine("Введённое число чётное");
+else Console.WriteLine("Введённое число нечётное");
+
+// Console.WriteLine(a % 2 == 0 ? "Чётное" : "Не чётное"); - Вариант решения из инетренета
