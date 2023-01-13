15d08bdbfcf2aaf538ffa94f24addf33e615102a
_DG_ <damirgaynanov@gmail.com>
2023-01-14 02:05:04 +0400

    Four commit

---
+++ Task_008/Program.cs
@@ -0,0 +1,18 @@
+﻿// Requrst for input data
+Console.WriteLine("Enter the number");
+string number = Console.ReadLine() ?? "";
+
+// Processing of input data
+int a = int.Parse(number);
+int b = 1;
+// Decision
+while (b < a)
+{
+    if (b < a)
+    {
+        if (b % 2 == 0) Console.Write(b + " ");
+    }
+
+    else Console.Write(b);
+    b = b + 1;
+}
