128b3c53fe97b2d415ed76e3dfe0643086802a34
_DG_ <damirgaynanov@gmail.com>
2023-01-17 00:36:14 +0400

    Исправлен текст вывода

--- Task_006/Program.cs
+++ Task_006/Program.cs
@@ -7,6 +7,6 @@ int a = int.Parse(num);
 
 // Decision
 if (a % 2 == 0) Console.WriteLine("Введённое число чётное");
-else Console.WriteLine("Введённое число нечётное");
+else Console.WriteLine("Введённое не является чётным");
 
 // Console.WriteLine(a % 2 == 0 ? "Чётное" : "Не чётное"); - Вариант решения из инетренета
