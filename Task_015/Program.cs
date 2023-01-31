0a0c4dfdffadb1eb6cb28cb7323662c44033bbf9
_DG_ <damirgaynanov@gmail.com>
2023-01-31 20:53:00 +0400

    Fixed

--- Task_015/Program.cs
+++ Task_015/Program.cs
@@ -6,10 +6,29 @@
 // 1 -> нет
 Console.WriteLine("Ввеlите цифру");
 int number = Convert.ToInt32(Console.ReadLine());
-bool dayOff = DayOff(number);
-Console.WriteLine(dayOff ? "Да": "Нет");
+if (number > 7 || number < 1) Console.WriteLine("Некорректный ввод. Введите число от 1 до 7");
+else 
+{
+    bool dayOff = DayOff(number);
+    Console.WriteLine(dayOff ? "Да" : "Нет");
+}
 
 bool DayOff(int num)
 {
-    return(num == 6 || num == 7);
+    return (num == 6 || num == 7);
 }
+
+
+
+// bool DayOff(int num)
+// {
+//     return(num == 6 || num == 7);
+// }
+
+// bool dayOff = DayOff(number);
+// Console.WriteLine(number > 7 || number < 1 ? Console.WriteLine(dayOff ? "Да": "Нет") : "Некорректный ввод. Введите число от 1 до 7");
+
+// bool DayOff(int num)
+// {
+//     return(num == 6 || num == 7);
+// }
