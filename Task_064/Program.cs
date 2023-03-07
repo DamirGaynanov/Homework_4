0d095f27f89e8231797352ad34bc2daf7ad36f14
_DG_ <damirgaynanov@gmail.com>
2023-03-08 02:14:51 +0300

    Done!

--- Task_064/Program.cs
+++ Task_064/Program.cs
@@ -7,13 +7,27 @@
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 Console.WriteLine("Введите натуральное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = UserInput();
+
 Console.Write($"{number} -> ");
 NaturalNumber(number);
+
 
 void NaturalNumber(int num)
 {
     if (num ==0) return;
     Console.Write($"{num} ");
     NaturalNumber(num-1);
+}
+
+void IncorrectValue()
+{
+    Console.WriteLine("Введено некорректное значение");
+    Environment.Exit(0);
+}
+
+int UserInput()
+{
+    if (!int.TryParse(Console.ReadLine(), out int temp)) IncorrectValue();
+    return temp;
 }
